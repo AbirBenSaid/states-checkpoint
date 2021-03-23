444731fffc73d59606a976e23a71463c787ca363
AbirBenSaid <78824029+AbirBenSaid@users.noreply.github.com>
2021-03-23 15:29:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,26 +36,3 @@ export default App
 
 
 
-
-//export default class App extends React.Component {
-
-//  handleClick=()=>this.setState({shown:!(this.state.shown)});
-  
-//  render() {
-//    return(
-//      <React.Fragment>
-//        <div className={this.state.shown? "appear":"dissapear"}>
-//          <h1 style={{fontStyle:'italic'}}>Hello my name is {this.state.fullName}</h1>
-//          <h2>here's my story {this.state.bio} </h2>
-//        <React.Fragment>
-//           <img style={{borderRadius: '8px'}} src={this.state.src} alt=''/>
-//          </React.Fragment>
-//          <h2>I am a {this.state.profession} </h2>
-//        </div>
-//        <div className="appear">
-//          <button  onClick={this.handleClick}> Show/Hide </button>
-//        </div>
-//      </React.Fragment>
-//    )
-//  }
-// }
